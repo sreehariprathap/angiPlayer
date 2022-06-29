343d46d4ee195f1061f3f948ab07f34dd2c13f65
Sreehari Prathap <sreehariprathap1996@gmail.com>
2022-06-29 15:36:40 +0530

    starter setup -- routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = []
+
 @NgModule({
-  imports: [],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
